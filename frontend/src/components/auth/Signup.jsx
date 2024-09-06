5e1ea600ecff8cfbfe1b792ffaec02da66092176
Krishna Kumar <krishnakumar750514@gmail.com>
2024-09-06 18:04:38 +0530

    added component/auth

---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              placeholder="Enter your fullname"
+              name="fullname"
+              value={input.fullname}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input
+              type="number"
+              placeholder="Enter your phone number"
+              name="phoneNumber"
+              value={input.phoneNumber}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+              <Label>Profile</Label>
+              <Input
+                accept="image/*"
+                type="file"
+                className="cursor-pointer"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+          <Button type="submit" className="w-full my-4">
+            SignUp
+          </Button>
+          <span className="text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
