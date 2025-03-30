9719263cc338eb8bb76bce2e350ef90e70bef5b5
Krishna-Kumar-18 <krishnakumar750514@gmail.com>
2025-03-30 23:49:47 +0530

    refactor(app): update API endpoints to use Render URL

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOption = {
-    origin:'http://localhost:5173', 
+    origin:'https://job-listing-portal-61em.onrender.com', 
     credentials : true
 }
 
